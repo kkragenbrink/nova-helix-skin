a01ca8797bed447d94b6651d59a7ca1c8d8f5ee4
Kevin Kragenbrink <kevin@writh.net>
2014-03-21 02:30:47 -0700

    Finalized login panel and added header image.

--- template_header.php
+++ template_header.php
@@ -67,31 +67,37 @@ $login_button = array(
 </ul>
 
 <header>
-    <section id="content-header">
-        <section id="control-panel">
-        <?php if (true || !Auth::is_logged_in()): ?>
-            <?php echo form_open('login/check_login'); ?>
-            <span class="form-element input">
-                <label for="cp-signin-email"><?php echo ucwords(lang('labels_email_address')); ?></label>
-                <input type="email" name="email" id="cp-signin-email" />
-            </span>
-            <span class="form-element input">
-                <label for="cp-signin-password"><?php echo ucwords(lang('labels_password')); ?></label>
-                <input type="password" name="password" id="cp-signin-password" />
-            </span>
-            <span class="form-element checkbox">
-                <input type="checkbox" name="remember" id="cp-signin-remember" value="yes" />
-                <label for="cp-signin-remember"><?php echo ucfirst(lang('actions_remember').' '.lang('labels_me')); ?></label>
-            </span>
-            <span class="form-element">
-                <?php echo anchor('login/reset_password', lang('login_forgot')); ?>
-            </span>
-            <span class="form-element button">
-                <?php echo form_button($login_button); ?>
-            </span>
-        <?php endif; ?>
+    <div>
+        <section id="content-header">
+            <section id="control-panel">
+            <?php if (true || !Auth::is_logged_in()): ?>
+                <?php echo form_open('login/check_login'); ?>
+                <section id="cp-signin-details">
+                    <span class="form-element input">
+                        <label for="cp-signin-email"><?php echo ucwords(lang('labels_email_address')); ?></label>
+                        <input type="email" name="email" id="cp-signin-email" />
+                    </span>
+                    <span class="form-element input">
+                        <label for="cp-signin-password"><?php echo ucwords(lang('labels_password')); ?></label>
+                        <input type="password" name="password" id="cp-signin-password" />
+                    </span>
+                    <span class="form-element button">
+                        <?php echo form_button($login_button); ?>
+                    </span>
+                </section>
+                <section id="cp-signin-tools">
+                    <span class="form-element checkbox">
+                        <input type="checkbox" name="remember" id="cp-signin-remember" value="yes" />
+                        <label for="cp-signin-remember"><?php echo ucfirst(lang('actions_remember').' '.lang('labels_me')); ?></label>
+                    </span>
+                    <span class="form-element">
+                        <?php echo anchor('login/reset_password', lang('login_forgot')); ?>
+                    </span>
+                </section>
+            <?php endif; ?>
+            </section>
         </section>
-    </section>
+    </div>
 </header>
 
 <section id="dashboard" class="hidden"></section>
